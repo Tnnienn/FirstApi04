648dbec584008be5c83adbc0c5ee2c74b93f4948
Antonio Fasanella <fasanella.antonio.sanfele@gmail.com>
2022-11-26 18:49:09 +0100

    Exercise implemented

--- src/main/java/co/develhope/FirstApi04/controllers/HeaderController.java
+++ src/main/java/co/develhope/FirstApi04/controllers/HeaderController.java
@@ -1,6 +1,8 @@
 package co.develhope.FirstApi04.controllers;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HeaderController {
 
     @GetMapping
-    public String getHeaders() {
-        return null;
+    public String getHeaders(@RequestHeader(HttpHeaders.HOST) String host) {
+        String[] hostArray = host.split(":");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Host Name: ").append(hostArray[0]);
+        sb.append("\nPort: ").append(hostArray[1]);
+        return sb.toString().trim();
     }
 }
